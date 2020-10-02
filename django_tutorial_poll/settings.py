cd5c5ec94d77b2655d6eebf1d12c37db0fb70cc8
becky112358 <8b2@protonmail.com>
2020-10-02 15:28:27 +0300

    Move the secret key into a separate file, so all other files can be publicly uploaded to github.

--- django_tutorial_poll/settings.py
+++ django_tutorial_poll/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .secrets import secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')6bmuj&d7fstu0@7)rh8eu^v@tkg(l5#m3c_(6i0tok$!h$)x9'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
